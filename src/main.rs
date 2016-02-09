c02f1602f3c594881a39296d759c21779e5f1c76
Eeo Jun <packwolf58@gmail.com>
2016-02-09 17:49:47 +0800

    Changed implementation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 use std::env;
-use std::collections::HashSet;
+use std::collections::{HashMap,BTreeMap};
 
-
-fn build_map(string: String) -> HashSet<String> {
-    let mut set = HashSet::new();
-    for s in string.split('\n') {
-        set.insert(s.to_string());
+fn build_map(string: String) -> HashMap<String,usize> {
+    let mut map = HashMap::new();
+    for (idx, line) in string.split('\n').enumerate() {
+        map.insert(line.to_string(), idx);
     }
-    return set;
+    return map;
 }
 
 fn intersections(string: String,
-                 mut set: HashSet<String>) -> HashSet<String> {
-    for s in string.split('\n') {
-        if !set.contains(s) {
-            set.remove(s);
+                 mut map: HashMap<String,usize>) -> HashMap<String,usize> {
+    for line in string.split('\n') {
+        if !map.contains_key(line) {
+            map.remove(line);
         }
     }
-    return set;
+    return map;
+}
+
+fn order_map(map: HashMap<String,usize>) -> BTreeMap<usize,String> {
+    let mut strs = BTreeMap::new();
+    for (line, idx) in map {
+        strs.insert(idx, line);
+    }
+    return strs;
 }
 
 fn main() {
@@ -28,18 +35,12 @@ fn main() {
     };
 
     let (_, rest) = args.split_at(2);
-    match rest.to_vec().pop() {
-        Some(text) => {
-            let mut set = build_map(first);
-            for string in rest {
-                set = intersections(string.to_owned(), set);
-            }
-            for line in text.split('\n') {
-                if set.contains(line) {
-                    println!("{}", line);
-                }
-            }
-        },
-        None => return
+    let mut map   = build_map(first);
+    for string in rest {
+        map = intersections(string.to_owned(), map);
+    }
+    let lines = order_map(map);
+    for line in lines.values() {
+        println!("{}", line);
     }
 }
