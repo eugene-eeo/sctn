9a6a2ee87652a223e8e0d744b21a6f65161a4581
Eeo Jun <packwolf58@gmail.com>
2016-02-10 10:55:16 +0800

    Added more complex test cases

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ def sh(args=[]):
     return check_output(['./target/debug/sctn'] + args)
 
 
+assert sh(['one\ntwo\nthree', 'one\n', 'one\n']) == b'one\n'
+assert sh(['one\ntwo\nthree', 'one\nfour']) == b'one\n'
 assert sh(['one', 'one']) == b'one\n'
 assert sh(['one', 'two']) == b''
 assert sh(['one']) == b''
