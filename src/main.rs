467903c0b8b41a099e89087f23f486d1291ae789
Eeo Jun <packwolf58@gmail.com>
2016-02-10 16:48:34 +0800

    Pass all tests

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,34 @@ use std::collections::HashSet;
 
 type StrHash<'a> = HashSet<&'a str>;
 
-fn build_set(string: &str) -> StrHash {
-    let mut set = HashSet::new();
-    for line in string.split('\n') {
-        set.insert(line);
-    }
-    return set;
+fn build_index(string: &str) -> StrHash {
+    string.split('\n').collect()
 }
 
-fn intersections<'a>(string: &str, mut set: StrHash<'a>) -> StrHash<'a> {
-    for line in string.split('\n') {
-        if !set.contains(line) {
-            set.remove(line);
-        }
-    }
-    return set;
+fn intersection<'a>(string: &'a str, index: &StrHash<'a>) -> StrHash<'a> {
+    let new = build_index(string);
+    index.intersection(&new)
+         .map(|x| *x)
+         .collect::<StrHash>()
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         return;
-    };
+    }
 
-    let ref first = args[1];
+    let ref first = args.get(1).unwrap();
     let (_, rest) = args.split_at(2);
-    let last = rest.to_vec().pop().unwrap();
+    let mut rest = rest.to_vec();
+    let last = rest.pop().unwrap();
 
-    let mut set: StrHash = build_set(&first);
-    for string in rest {
-        set = intersections(string, set);
+    let mut index = build_index(first);
+    for string in &rest {
+        index = intersection(string, &index);
     }
     for line in last.split('\n') {
-        if set.contains(line) {
+        if index.contains(line) {
             println!("{}", line);
         }
     }
