50fb2adfbe1090a9f63f4e91c322106aedc9becc
Eeo Jun <141bytes@gmail.com>
2016-06-10 09:04:06 +0800

    Parse arguments 'properly'

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::env;
+extern crate argparse;
+
 use std::collections::BTreeSet;
+use argparse::{ArgumentParser, Store, List};
 
 type StrSet<'a> = BTreeSet<&'a str>;
 
@@ -15,18 +17,23 @@ fn intersection<'a>(string: &'a str, index: &StrSet<'a>) -> StrSet<'a> {
 }
 
 fn main() {
-    let mut args: Vec<String> = env::args()
-        .skip(1)
-        .collect();
-    if args.len() < 2 {
-        return;
+    let mut first: String = "".to_string();
+    let mut strings: Vec<String> = vec![];
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Intersect outputs.");
+        ap.refer(&mut first)
+          .add_argument("first", Store, "string");
+        ap.refer(&mut strings)
+          .add_argument("strings", List, "strings to intersect");
+        ap.parse_args_or_exit();
     }
-
-    let last    = args.pop().unwrap();
-    let initial = args.pop().unwrap();
-
-    let mut index = build_index(&initial);
-    for string in &args {
+    let last = match strings.pop() {
+        Some(v) => v,
+        None    => return
+    };
+    let mut index = build_index(&first);
+    for string in &strings {
         index = intersection(string, &index);
     }
     for line in last.split('\n') {
